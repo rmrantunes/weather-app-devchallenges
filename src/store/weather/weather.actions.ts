8ccdb159415a615c36119695d18e877ba0eaa886
Rafael Antunes <rafaelmessias710@gmail.com>
2021-03-28 16:03:32 -0400

    🚀- weather reducer almost done

--- src/store/weather/weather.actions.ts
+++ src/store/weather/weather.actions.ts
@@ -1,7 +1,17 @@
-import { MetaWeatherAPIResponse } from "src/adapters/metaweather-api-definitions";
+import axios from "axios";
+import { Dispatch } from "react";
+import {
+  MetaWeatherAPIResponse,
+  MetaWeatherSearchResponse,
+} from "src/adapters/metaweather-api-definitions";
 
 export const weatherActionTypes = {
   SET_WEATHER: "weather/SET_WEATHER",
+  SEARCH_WEATHER: "weather/SEARCH_WEATHER",
+  SEARCH_WEATHER_STARTED: "weather/SEARCH_WEATHER_STARTED",
+  SEARCH_WEATHER_ERROR: "weather/SEARCH_WEATHER_ERROR",
+  GET_WEATHER_BY_WOEID: "weather/GET_WEATHER_BY_WOEID",
+  SET_LAST_SEARCHES: "weather/SET_LAST_SEARCHES",
 };
 
 export const weatherActions = {
@@ -11,12 +21,43 @@ export const weatherActions = {
       payload: weather,
     };
   },
-  // SEARCH_WEATHER(searchText: string) {
-  //   return async function(dispatch: Dispatch) {
-  //     const { data } = await axios.get<MetaWeatherSearchResponse[]>(
-  //       `/api/search/${searchText}`
-  //     );
-  //     dispatch(weatherActions.SET_WEATHER(data))
-  //   }
-  // }
+  SEARCH_WEATHER(searchText: string) {
+    return async function (dispatch) {
+      try {
+        dispatch(weatherActions.SEARCH_WEATHER_STARTED());
+        const { data } = await axios.get<MetaWeatherSearchResponse[]>(
+          `/api/search/${searchText}`
+        );
+        if (!data || data.length === 0)
+          throw new Error("Digite uma outra cidade");
+
+        const [{ title, woeid }] = data;
+        await dispatch(weatherActions.GET_WEATHER_BY_WOEID({ title, woeid }));
+      } catch (error) {
+        dispatch(weatherActions.SEARCH_WEATHER_ERROR(error.message));
+      }
+    };
+  },
+  SEARCH_WEATHER_STARTED() {
+    return { type: weatherActionTypes.SEARCH_WEATHER_STARTED };
+  },
+  SEARCH_WEATHER_ERROR(error: string) {
+    return { type: weatherActionTypes.SEARCH_WEATHER_ERROR, payload: error };
+  },
+  GET_WEATHER_BY_WOEID(payload: { woeid: number; title: string }) {
+    try {
+      return async function (dispatch) {
+        const { data } = await axios.get<MetaWeatherAPIResponse>(
+          `/api/location/${payload.woeid}`
+        );
+        dispatch(weatherActions.SET_WEATHER(data));
+        dispatch(weatherActions.SET_LAST_SEARCHES(payload));
+      };
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  SET_LAST_SEARCHES(payload: { woeid: number; title: string }) {
+    return { type: weatherActionTypes.SET_LAST_SEARCHES, payload };
+  },
 };
--- src/store/weather/weather.reducer.ts
+++ src/store/weather/weather.reducer.ts
@@ -1,26 +1,50 @@
 import { MetaWeatherAPIResponse } from "src/adapters/metaweather-api-definitions";
-import { weatherActionTypes } from "./weather.actions";
+import { weatherActions, weatherActionTypes } from "./weather.actions";
 
 interface Action {
   type: string;
-  payload: MetaWeatherAPIResponse;
+  payload: any;
 }
 
 interface WeatherState {
   weather: MetaWeatherAPIResponse | null;
+  lastSearches: MetaWeatherAPIResponse[];
+  loading: boolean;
+  error: string | null;
 }
 
 const initialState: WeatherState = {
   weather: null,
+  loading: false,
+  error: null,
+  lastSearches: [],
 };
 
 export function weatherReducer(
   state: WeatherState = initialState,
   action: Action
-) {
+): WeatherState {
   switch (action.type) {
     case weatherActionTypes.SET_WEATHER:
-      return { ...state, weather: action.payload };
+      return {
+        ...state,
+        weather: action.payload as MetaWeatherAPIResponse,
+        loading: false,
+      };
+    case weatherActionTypes.SEARCH_WEATHER_STARTED:
+      return { ...state, loading: true, error: null };
+    case weatherActionTypes.SEARCH_WEATHER_ERROR:
+      return { ...state, loading: false, error: action.payload as string };
+    case weatherActionTypes.SET_LAST_SEARCHES:
+      const isNewWoeid = state.lastSearches.every(
+        ({ woeid: currentWoeid }) => currentWoeid !== action.payload.woeid
+      );
+      return {
+        ...state,
+        lastSearches: isNewWoeid
+          ? [...state.lastSearches, action.payload]
+          : state.lastSearches,
+      };
     default:
       return state;
   }
